27eccf0ed535c177dd949efbd8858157de553aff
redakker <redman@redman.hu>
2022-03-13 09:47:47 +0100

    web tools upload update

--- src/webcontent.h
+++ src/webcontent.h
@@ -372,7 +372,7 @@ BLEcker
 <a class="button w100" href="/">home</a>
 <a class="button w100" href="/update">update</a>
 <!--<a class="button w100 button-danger" id="reset" href="#">reset</a>-->
-<div class="version" id="version">v1.06 - 86</div>
+<div class="version" id="version">v1.07 - 88</div>
 <div id="footer">
 <div><a href="https://github.com/redakker/blecker" target="_blank">blecker</a></div>
 </div>
@@ -1513,7 +1513,7 @@ BLEcker
 <a class="button w100" href="/">home</a>
 <a class="button w100" href="/update">update</a>
 <!--<a class="button w100 button-danger" id="reset" href="#">reset</a>-->
-<div class="version" id="version">v1.06 - 86</div>
+<div class="version" id="version">v1.07 - 88</div>
 <div id="footer">
 <div><a href="https://github.com/redakker/blecker" target="_blank">blecker</a></div>
 </div>
